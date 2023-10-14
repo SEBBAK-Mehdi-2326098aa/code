986ac9f81339bebbb2e60374f05d89ff127358ca
WMehdi <wmehdi@Mehdi.myguest.virtualbox.org>
2023-10-14 14:41:35 +0200

    je teste un dernier truc

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string alpha;
+    string question = "Est tu Un Alpha";
+    unsigned reponse;
+    alpha= "Je suis le meilleur gars au monde";
+
+    cout << alpha << endl;
+    cout << question;
+    cin >> reponse;
+
+        if (reponse==1){
+        cout << "bien joué tu en est vraiment un" << endl;}
+    else
+
+        cout << "looser va" << endl;
+
+
+    return 0;
+}
